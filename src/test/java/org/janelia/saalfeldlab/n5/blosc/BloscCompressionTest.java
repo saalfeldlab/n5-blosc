9978b4e06ac3f590f2161c7e63e35744ea90de2d
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2019-07-30 13:22:21 +0200

    rename test to BloscCompressionTest

--- src/test/java/org/janelia/saalfeldlab/n5/blosc/BloscCompressionTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/blosc/BloscCompressionTest.java
@@ -23,10 +23,39 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+package org.janelia.saalfeldlab.n5.blosc;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.blosc.JBlosc;
+import org.janelia.saalfeldlab.n5.AbstractN5Test;
+import org.janelia.saalfeldlab.n5.Compression;
+import org.janelia.saalfeldlab.n5.N5FSWriter;
+import org.janelia.saalfeldlab.n5.N5Writer;
+
 /**
- * Blosc compression for N5
+ * Lazy {@link BloscCompression} test using the abstract base class.
  *
  * @author Stephan Saalfeld &lt;saalfelds@janelia.hhmi.org&gt;
- *
  */
-package org.janelia.saalfeldlab.n5.blosc;
+public class BloscCompressionTest extends AbstractN5Test {
+
+	static private String testDirPath = System.getProperty("user.home") + "/tmp/n5-test";
+
+	/**
+	 * @throws IOException
+	 */
+	@Override
+	protected N5Writer createN5Writer() throws IOException {
+
+		return new N5FSWriter(testDirPath);
+	}
+
+	@Override
+	protected Compression[] getCompressions() {
+
+		return Arrays.stream(new JBlosc().listCompressors().split(",")).map(
+				a -> new BloscCompression(a, 6, BloscCompression.SHUFFLE, 0, 1)).toArray(BloscCompression[]::new);
+	}
+}
